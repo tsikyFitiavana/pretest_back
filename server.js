ae631f9dbc4d0ea7ee4b2f102dc921b5aa261f2d
tsikyFitiavana <tsikybr@gmail.com>
2019-07-15 16:34:53 +0300

    registar fait

---
+++ server.js
@@ -0,0 +1,45 @@
+const PORT = process.env.PORT || 8080
+//Définition des modules
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+
+//Connexion à la base de donnée
+mongoose.connect('mongodb://localhost/pretest', {
+    useCreateIndex: true,
+    useNewUrlParser: true
+}).then(() => {
+    console.log('Connected to mongoDB')
+}).catch(e => {
+    console.log('Error while DB connecting');
+    console.log(e);
+});
+
+//On définit notre objet express nommé app
+const app = express();
+
+//Body Parser
+const urlencodedParser = bodyParser.urlencoded({
+    extended: true
+});
+app.use(urlencodedParser);
+app.use(bodyParser.json());
+
+//Définition des CORS
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+//On définit la route Hello
+//Définition du routeur
+var router = express.Router();
+app.use('/user', router);
+require(__dirname + '/controllers/userController')(router);
+
+//Définition et mise en place du port d'écoute
+
+app.listen(PORT, () => console.log('server demarre avec port ' + PORT));
